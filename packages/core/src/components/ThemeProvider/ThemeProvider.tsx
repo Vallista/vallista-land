d42a135395e2a2498f95870749879bc31d64c19e
vallista <mgh950714@gmail.com>
2021-12-07 02:14:18 +0900

    feat: Reset CSS (emotion global style) 적용

--- packages/core/src/components/ThemeProvider/ThemeProvider.tsx
+++ packages/core/src/components/ThemeProvider/ThemeProvider.tsx
@@ -1,5 +1,6 @@
-import { ThemeProvider as BaseThemeProvider } from '@emotion/react'
-import React, { FC } from 'react'
+import { Global, ThemeProvider as BaseThemeProvider, css } from '@emotion/react'
+import styled from '@emotion/styled'
+import React, { FC, VFC } from 'react'
 
 import { BaseThemeMapper, Colors, Layers } from './type'
 
@@ -30,5 +31,148 @@ type ThemeKeys = keyof typeof Themes
  * ```
  */
 export const ThemeProvider: FC<{ theme?: ThemeKeys }> = ({ theme = 'DEFAULT', children }) => {
-  return <BaseThemeProvider theme={Themes[theme]}>{children}</BaseThemeProvider>
+  return (
+    <>
+      <Reset />
+      <BaseThemeProvider theme={Themes[theme]}>{children}</BaseThemeProvider>
+    </>
+  )
+}
+
+const Reset: VFC = () => {
+  return (
+    <Global
+      styles={css`
+        /* http://meyerweb.com/eric/tools/css/reset/ 
+    v2.0 | 20110126
+    License: none (public domain)
+  */
+
+        html,
+        body,
+        div,
+        span,
+        applet,
+        object,
+        iframe,
+        h1,
+        h2,
+        h3,
+        h4,
+        h5,
+        h6,
+        p,
+        blockquote,
+        pre,
+        a,
+        abbr,
+        acronym,
+        address,
+        big,
+        cite,
+        code,
+        del,
+        dfn,
+        em,
+        img,
+        ins,
+        kbd,
+        q,
+        s,
+        samp,
+        small,
+        strike,
+        strong,
+        sub,
+        sup,
+        tt,
+        var,
+        b,
+        u,
+        i,
+        center,
+        dl,
+        dt,
+        dd,
+        ol,
+        ul,
+        li,
+        fieldset,
+        form,
+        label,
+        legend,
+        table,
+        caption,
+        tbody,
+        tfoot,
+        thead,
+        tr,
+        th,
+        td,
+        article,
+        aside,
+        canvas,
+        details,
+        embed,
+        figure,
+        figcaption,
+        footer,
+        header,
+        hgroup,
+        menu,
+        nav,
+        output,
+        ruby,
+        section,
+        summary,
+        time,
+        mark,
+        audio,
+        video {
+          margin: 0;
+          padding: 0;
+          border: 0;
+          font-size: 100%;
+          font: inherit;
+          vertical-align: baseline;
+        }
+        /* HTML5 display-role reset for older browsers */
+        article,
+        aside,
+        details,
+        figcaption,
+        figure,
+        footer,
+        header,
+        hgroup,
+        menu,
+        nav,
+        section {
+          display: block;
+        }
+        body {
+          line-height: 1;
+        }
+        ol,
+        ul {
+          list-style: none;
+        }
+        blockquote,
+        q {
+          quotes: none;
+        }
+        blockquote:before,
+        blockquote:after,
+        q:before,
+        q:after {
+          content: '';
+          content: none;
+        }
+        table {
+          border-collapse: collapse;
+          border-spacing: 0;
+        }
+      `}
+    />
+  )
 }
