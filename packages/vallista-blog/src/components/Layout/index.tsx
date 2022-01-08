7741dfad481fc46008aba45ef9e4cc2ac8e5137f
마광휘 <mgh950714@gmail.com>
2022-01-09 01:05:03 +0900

    feat: 푸터 짤라지는 버그 수정

--- packages/vallista-blog/src/components/Layout/index.tsx
+++ packages/vallista-blog/src/components/Layout/index.tsx
@@ -44,37 +44,39 @@ export const Layout: FC = (props) => {
         <Header fold={fold} folding={handleFold} />
         <Main fold={fold}>
           <Article>{children}</Article>
-          <FooterBox>
-            <Footer>
-              <FooterGroup title='사이트 맵'>
-                <FooterLink custom>
-                  <Link to='/'>홈</Link>
-                </FooterLink>
-                <FooterLink custom>
-                  <Link to='/posts'>포스트</Link>
-                </FooterLink>
-                <FooterLink custom>
-                  <Link to='/resume'>이력서</Link>
-                </FooterLink>
-              </FooterGroup>
-              <FooterGroup title='관련 사이트'>
-                <FooterLink href='https://vallista.tistory.com'>다른 블로그</FooterLink>
-                <FooterLink href='https://career.woowahan.com/'>우아한형제들 채용</FooterLink>
-                <FooterLink href='https://techblog.woowahan.com/'>우아한형제들 기술블로그</FooterLink>
-              </FooterGroup>
-            </Footer>
-          </FooterBox>
-          <FooterAllRightReserve>
-            <Text size={14}>
-              Copyright ⓒ 2021 <Link to='https://vallista.kr'>Vallista</Link> All rights reserved.
-            </Text>
-            <Spacer y={0.1} />
-            <Text size={14}>
-              Created by <Link to='https://vallista.kr'>@Vallista</Link>. Powered By{' '}
-              <a href='https://github.com/Vallista/vallista-land'>@Vallista-land</a>
-            </Text>
-            <Spacer y={0.5} />
-          </FooterAllRightReserve>
+          <div>
+            <FooterBox>
+              <Footer>
+                <FooterGroup title='사이트 맵'>
+                  <FooterLink custom>
+                    <Link to='/'>홈</Link>
+                  </FooterLink>
+                  <FooterLink custom>
+                    <Link to='/posts'>포스트</Link>
+                  </FooterLink>
+                  <FooterLink custom>
+                    <Link to='/resume'>이력서</Link>
+                  </FooterLink>
+                </FooterGroup>
+                <FooterGroup title='관련 사이트'>
+                  <FooterLink href='https://vallista.tistory.com'>다른 블로그</FooterLink>
+                  <FooterLink href='https://career.woowahan.com/'>우아한형제들 채용</FooterLink>
+                  <FooterLink href='https://techblog.woowahan.com/'>우아한형제들 기술블로그</FooterLink>
+                </FooterGroup>
+              </Footer>
+            </FooterBox>
+            <FooterAllRightReserve>
+              <Text size={14}>
+                Copyright ⓒ 2021 <Link to='https://vallista.kr'>Vallista</Link> All rights reserved.
+              </Text>
+              <Spacer y={0.1} />
+              <Text size={14}>
+                Created by <Link to='https://vallista.kr'>@Vallista</Link>. Powered By{' '}
+                <a href='https://github.com/Vallista/vallista-land'>@Vallista-land</a>
+              </Text>
+              <Spacer y={0.5} />
+            </FooterAllRightReserve>
+          </div>
         </Main>
       </Container>
     </Wrapper>
