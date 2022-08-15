a91ef8c2e7b3739de465782ce52af10d83f3ad7b
Vallista <mgh950714@gmail.com>
2022-08-15 15:10:21 +0900

    ♻️ Series 컴포넌트 스타일 파일 분리

--- packages/vallista-blog/src/components/Series/index.tsx
+++ packages/vallista-blog/src/components/Series/index.tsx
@@ -1,8 +1,8 @@
-import { css } from '@emotion/react'
-import styled from '@emotion/styled'
 import { Collapse, Container } from '@vallista-land/core'
 import { navigate } from 'gatsby'
 import { VFC } from 'react'
+
+import * as Styled from './Series.style'
 
 interface SeriesProps {
   name: string
@@ -19,13 +19,13 @@ export const Series: VFC<SeriesProps> = (props) => {
   return (
     <Collapse title={name} card size='medium' subtitle={`시리즈의 글 (${posts.length}개)`}>
       <Container>
-        <List>
+        <Styled._List>
           {posts.map((it) => (
-            <Item timeToRead={it.timeToRead} key={it.name}>
+            <Styled._Item timeToRead={it.timeToRead} key={it.name}>
               <span onClick={() => moveToLocation(it.slug)}>{it.name}</span>
-            </Item>
+            </Styled._Item>
           ))}
-        </List>
+        </Styled._List>
       </Container>
     </Collapse>
   )
@@ -34,41 +34,3 @@ export const Series: VFC<SeriesProps> = (props) => {
     navigate(slug)
   }
 }
-
-const List = styled.ol`
-  padding-left: 1.5rem;
-  box-sizing: border-box;
-  line-height: 1.6;
-  list-style: decimal;
-`
-
-const Item = styled.li<{ timeToRead: number }>`
-  ${({ theme, timeToRead }) => css`
-    margin-bottom: 0.5rem;
-    &::marker {
-      font-weight: 600;
-      color: ${theme.colors.HIGHLIGHT.PINK};
-    }
-
-    &::after {
-      content: '- ${timeToRead}분';
-      margin-left: 0.2rem;
-      font-size: 0.8rem;
-    }
-
-    & > span {
-      cursor: pointer;
-      border-bottom: 2px solid ${theme.colors.HIGHLIGHT.PINK};
-      font-weight: 600;
-      text-decoration: none;
-      color: ${theme.colors.PRIMARY.FOREGROUND};
-      transition: all 0.1s ease-out;
-
-      &:hover {
-        background: ${theme.colors.HIGHLIGHT.PINK};
-        border-top: 2px solid ${theme.colors.HIGHLIGHT.PINK};
-        color: ${theme.colors.PRIMARY.BACKGROUND};
-      }
-    }
-  `}
-`
