ecd90cd076251ebe7e9d6aca9ba3fcb054c1a3da
mgh <mgh@mgh.local>
2024-12-30 01:03:20 +0900

    feat: .github > workflows > node.js.yml 정상동작하게끔 수정

--- services/blog/gatsby-config.js
+++ services/blog/gatsby-config.js
@@ -17,23 +17,25 @@ module.exports = {
   plugins: [
     `gatsby-plugin-pnpm`,
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: 'gatsby-plugin-google-gtag',
       options: {
         // The property ID; the tracking code won't be generated without it
-        trackingId: 'UA-143764638-1',
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: false,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Delays sending pageview hits on route update (in milliseconds)
-        pageTransitionDelay: 0,
-        defer: false,
-        // Any additional optional fields
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
-        cookieDomain: 'vallista.kr'
+        trackingIds: ['UA-143764638-1'],
+        gtagConfig: {
+          anonymize_ip: true,
+          cookie_expires: 0,
+          cookie_name: 'vallista.kr',
+          sample_rate: 5
+        },
+        pluginConfig: {
+          // Defines where to place the tracking script - `true` in the head and `false` in the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Delays sending pageview hits on route update (in milliseconds)
+          delayOnRouteUpdate: 0,
+          origin: 'vallista.kr'
+        }
       }
     },
     'gatsby-plugin-emotion',
