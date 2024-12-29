505664e780b70767c537ef1afb3ac35890a9655f
mgh <mgh@mgh.local>
2024-12-30 00:50:33 +0900

    feat: scripts 추가 및 github actions ci 수정

---
+++ services/blog/scripts/deploy.js
@@ -0,0 +1,16 @@
+const TOKEN = process.argv[2] || null
+
+const ghpages = require('gh-pages')
+
+ghpages.publish(
+  'public',
+  {
+    branch: 'main',
+    repo: TOKEN
+      ? `https://vallista:${TOKEN}@github.com/Vallista/vallista.github.io.git`
+      : 'https://github.com/Vallista/vallista.github.io.git'
+  },
+  function (err) {
+    console.log(err)
+  }
+)
