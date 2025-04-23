fb59e51a0c81fb29ea11a16010bce24c60079177
Vallista <0x109071072@gmail.com>
2025-04-23 19:10:23 +0900

    fix: 모바일 -> 데스크탑 이동 시 사이드바 문제 해결

--- services/vite-blog/src/apps/layout/components/Sidebar/useSidebar.ts
+++ services/vite-blog/src/apps/layout/components/Sidebar/useSidebar.ts
@@ -1,4 +1,5 @@
 import { useGlobalProvider } from '@/context/useProvider'
+import { useMediaQuery } from '@/hooks/useMediaQuery'
 import { useScrollTo } from '@/hooks/useScrollTo'
 import { useEffect, useState } from 'react'
 
@@ -6,27 +7,42 @@ export const useSidebar = () => {
   const { state } = useGlobalProvider()
   const [scroll, setScroll] = useState<number>()
   const { scrollTo } = useScrollTo()
+  const isDesktop = useMediaQuery('(min-width: 1025px)')
+
+  const openMobileSidebar = () => {
+    const scrollY = window.scrollY
+    document.body.style.overflow = 'hidden'
+    document.body.style.position = 'fixed'
+    document.body.style.zIndex = '0'
+    document.body.style.width = '100%'
+    document.body.style.height = '100%'
+    document.getElementsByTagName('main')[0].style.top = `-${scrollY}px`
+    setScroll(scrollY)
+  }
+
+  const closeMobileSidebar = () => {
+    document.body.style.overflow = ''
+    document.body.style.position = ''
+    document.body.style.top = ''
+    document.body.style.width = ''
+    document.body.style.height = ''
+    document.getElementsByTagName('main')[0].style.top = '0'
+    scrollTo(scroll || 0, false)
+  }
 
   useEffect(() => {
-    if (state.mobileSidebarVisible) {
-      const scrollY = window.scrollY
-      setScroll(scrollY)
-      document.body.style.overflow = 'hidden'
-      document.body.style.position = 'fixed'
-      document.body.style.zIndex = '0'
-      document.body.style.width = '100%'
-      document.body.style.height = '100%'
-      document.getElementsByTagName('main')[0].style.top = `-${scrollY}px`
-    } else {
-      document.body.style.overflow = ''
-      document.body.style.position = ''
-      document.body.style.top = ''
-      document.body.style.width = ''
-      document.body.style.height = ''
-      document.getElementsByTagName('main')[0].style.top = '0'
-      scrollTo(scroll || 0, false)
-    }
+    if (state.mobileSidebarVisible) openMobileSidebar()
+    else closeMobileSidebar()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [state.mobileSidebarVisible])
+
+  useEffect(() => {
+    if (!isDesktop) return
+
+    // 데스크탑 모드인데 사이드바가 켜져있으면
+    if (state.mobileSidebarVisible) closeMobileSidebar()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isDesktop])
 
   return {
     fold: state.fold,
