0fe48d7d37a612da838ed6d7604131a4d3ab69b9
Vallista <0x109071072@gmail.com>
2025-04-28 17:45:04 +0900

    fix: 이전 캐시된 포스트의 경우 스크롤 상위로 올라가지 않는 문제 해결

--- services/blog/src/apps/contents/components/ContentLayout/index.tsx
+++ services/blog/src/apps/contents/components/ContentLayout/index.tsx
@@ -7,23 +7,26 @@ import { Comment } from '@/apps/layout/components/Comment'
 
 import { useContentLayout } from './useContentLayout'
 import { Loading } from './index.style'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useScrollTo } from '@/hooks/useScrollTo'
 
 export const ContentLayout = () => {
   const { articleHeight, articleSeriesHeight, isPageReady, contentWithRaw, slug } = useContentLayout()
   const { scrollToTop } = useScrollTo()
+  const [nowSlug, setSlug] = useState(slug)
 
   const EmptyLayout = Loading(articleHeight, articleSeriesHeight)
 
   useEffect(() => {
-    if (isPageReady) {
+    if (isPageReady && nowSlug === slug) {
       document.body.style.overflow = ''
     } else {
       scrollToTop(false)
       document.body.style.overflow = 'hidden'
     }
-  }, [isPageReady])
+
+    setSlug(slug)
+  }, [isPageReady, slug])
 
   return (
     <MDXProvider>
