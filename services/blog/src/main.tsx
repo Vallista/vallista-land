31d8e078eb7dce211dd10d84663a4b8e3dd48eae
Vallista <0x109071072@gmail.com>
2025-05-10 17:34:01 +0900

    fix: hydration을 통한 ssr 구현

--- services/blog/src/main.tsx
+++ services/blog/src/main.tsx
@@ -1,5 +1,5 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { hydrateRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 import { ThemeProvider } from '@vallista/design-system'
@@ -7,7 +7,8 @@ import App from './app'
 
 const queryClient = new QueryClient()
 
-createRoot(document.getElementById('root')!).render(
+hydrateRoot(
+  document.getElementById('root')!,
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <ThemeProvider>
